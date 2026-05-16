023349b3ca3f082c7acbe90b1d14812ea63e847a
Ashish sharma <ashishsharma@Ashishs-MacBook-Air.local>
2026-05-16 11:00:01 +0530

    auth API added

--- src/app.js
+++ src/app.js
@@ -2,15 +2,32 @@ const express = require("express");
 const { IsAuth } = require("./Middlewares/auth");
 const userModel = require("./models/user.model");
 const connectDB = require("./config/db");
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
 
 app.post("/public/user", async (req, res) => {
   try {
-    const user = new userModel(req.body);
+    const { firstName, lastName, email, password, age, skill } = req.body;
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = new userModel({
+      firstName,
+      lastName,
+      email,
+      password: hashPassword,
+      age,
+      skill,
+    });
 
-    const createUser = await user.save();
+    const createUser = await user.save({
+      firstName,
+      lastName,
+      email,
+      password,
+      age,
+      skill,
+    });
 
     res.status(201).json({
       status: "success",
@@ -59,12 +76,40 @@ app.delete("/user/:id", async (req, res) => {
   }
 });
 
+app.post("/public/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      res.status(404).json({ status: "failed", message: "user not found" });
+    }
+
+    const isValidUser = await bcrypt.compare(password, user.password);
+    console.log(isValidUser);
+
+    if (!isValidUser) {
+      res
+        .status(401)
+        .json({ status: "failed", message: "invalid credentials" });
+    } else {
+      res
+        .status(200)
+        .json({ status: "success", message: "Login successfully" });
+    }
+  } catch (err) {
+    res.status(500).json({ status: "Failed", message: "Something went wrong" });
+  }
+});
+
 connectDB()
   .then(() => {
+    console.log("DB connected");
     app.listen(8080, () => {
       console.log("server is running on 8080");
     });
   })
   .catch((err) => {
-    console.log("error in DB connection");
+    console.log("error in DB connection", err.message);
   });
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,21 +1,57 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+      minLength: 4,
+      maxLength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email");
+        }
+      },
+    },
+    password: {
+      type: String,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("password is not strong");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 60,
+    },
+    skill: {
+      type: [String],
+      validate(value) {
+        if (value.length > 5) {
+          throw new Error("limit exceeded for skills");
+        }
+      },
+    },
   },
-  lastName: {
-    type: String,
+  {
+    timestamps: true,
+    version: true,
   },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-});
+);
 
 module.exports = mongoose.model("User", userSchema);
