993650b112e883e1c174f6fc99aee7da678c8211
Ashish sharma <ashishsharma@Ashishs-MacBook-Air.local>
2026-06-02 14:01:03 +0530

    feed api is done

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { IsAuth } = require("../Middlewares/auth");
 const connectionModel = require("../models/sendConnection.model");
+const userModel = require("../models/user.model")
 
 const userRouter = express.Router();
 
@@ -41,5 +42,34 @@ userRouter.get("/user/connections", IsAuth, async (req, res) => {
     return res.status(500).json({ status: "failed", message: err.message });
   }
 });
+
+userRouter.get("/feed",IsAuth, async (req,res)=>{
+
+
+    try{
+          const loggedInUser = req.user.user
+          const page = req.query.page  || 1
+          const limit = req.query.limit || 10
+
+          const skip = (page - 1) * limit
+          
+
+          const feedList = await connectionModel.find({$or:[{fromUser:loggedInUser},{toUser:loggedInUser}]}).select("fromUser toUser")
+
+          const hideusersFromFeed = new Set()
+
+          feedList.forEach((req)=>{
+            hideusersFromFeed.add(req.fromUser.toString());
+            hideusersFromFeed.add(req.toUser.toString());
+          })
+
+          const users = await userModel.find({_id: {$nin: Array.from(hideusersFromFeed)}}).select("firstName lastName").skip(skip).limit(limit)
+
+          res.status(200).json({status:"success", data: users})
+
+    }catch(err){
+        return res.status(500).json({status:"failed",message:err.message})
+    }
+})
 
 module.exports = userRouter;
